40931f1dd547749b6926b513fcba04bc25d63cbe
ivan dobry <ivandobry@hotmail.es>
2023-07-31 10:51:32 +0200

    add jump

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,13 +9,17 @@ public class InputManager : MonoBehaviour
     private PlayerInput.OnFootActions onFoot;
 
     private PlayerMotor motor;
+    private PlayerLook look;
 
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+
         motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
         onFoot.Jump.performed += ctx => motor.Jump();
     }
 
@@ -23,6 +27,11 @@ public class InputManager : MonoBehaviour
     void FixedUpdate()
     {
         motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
